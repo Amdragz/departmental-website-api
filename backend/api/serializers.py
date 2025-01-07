da1caeb22cfe5a50d1f039f42c73093d0fdf7ea4
Ahmed Ogaji <ogaji2006@gmail.com>
2025-01-07 12:24:51 +0100

    feat(*): implemented the view for all the other models

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import CustomUser
+from .models import Course, CustomUser, Document, Notification, Profile, Resource
 
 class CustomUserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -13,7 +13,7 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user = CustomUser(
             username=validated_data['username'],
             matricno=validated_data['matricno'],
-            phonenumber=validated_data['phonenumber'],
+            phonenumber=validated_data['level'],
             email=validated_data['email'],
             department=validated_data['department'],
         )
@@ -21,3 +21,40 @@ class CustomUserSerializer(serializers.ModelSerializer):
         user.set_password(validated_data['password'])
         user.save()
         return user
+    
+class DocumentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Document
+        fields = '__all__'
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+class ResourceSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Resource
+        fields = '__all__'
+
+class NotificationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notification
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    registered_courses = CourseSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Profile
+        fields = [
+            'user',
+            'registered_courses',
+            'total_units',
+            'level',
+            'phone_number',
+            'about',
+            'department',
+            'matric_number',
+        ]
+
