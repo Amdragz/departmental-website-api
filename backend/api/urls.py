f943356712b37022410fddb744e3c6b2e36832eb
Ahmed Ogaji <ogaji2006@gmail.com>
2025-01-31 23:33:54 +0100

    Fix internal server error on resources  api endpoint call

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path("users/<user_id>/", views.GetUserByMatricNoView.as_view(), name="get-authenticated-user"),
   path("users/documents/", views.DocumentViewSet.as_view(), name="get-documents"),
   path("users/resources/", views.ResourceViewSet.as_view(), name="get-resources"),
   path("users/courses/", views.CourseViewSet.as_view(), name="get-courses"),
   path("users/notifications/", views.NotificationViewSet.as_view(), name="get-notifications"),
+  path("users/<user_id>/", views.GetUserByMatricNoView.as_view(), name="get-authenticated-user"),
 ]
